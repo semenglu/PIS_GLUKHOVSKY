da5f712d52242846c92ef7bfde7b548f6ecc4a6f
Semen <semengluhovsky@mail.ru>
2024-11-10 20:04:03 +0300

    refactor: ЛАБ1/#7

--- lab1/3-9.py
+++ lab1/3-9.py
@@ -106,33 +106,38 @@ class BaseClient:
             parsed_data['comment'],
         )
 
+    def __eq__(self, other):
+        if not isinstance(other, BaseClient):
+            return NotImplemented
+        return (self.client_id == other.client_id and
+                self.surname == other.surname and
+                self.first_name == other.first_name and
+                self.patronymic == other.patronymic and
+                self.email == other.email and
+                self.phone_number == other.phone_number and
+                self.passport_number == other.passport_number)
     
 class FullClient(BaseClient):
-    def __init__(self, client_id, surname="", first_name="", patronymic="", comment=""):
-        super().__init__(client_id, surname, first_name, patronymic, comment)
+    def __init__(self, client_id, surname="", first_name="", patronymic="",email="", phone_number="", passport_number="", comment=""):
+        super().__init__(client_id, surname, first_name, patronymic, email, phone_number, passport_number, comment)
         
-        if not self.validate_client_id(self.client_id):
-            raise ValueError("Invalid client ID")
-        if not self.validate_text_field(self.surname, "surname"):
-            raise ValueError("Invalid surname")
-        if not self.validate_text_field(self.first_name, "first name"):
-            raise ValueError("Invalid first name")
-        if not self.validate_text_field(self.patronymic, "patronymic"):
-            raise ValueError("Invalid patronymic")
-        if not self.validate_comment(self.comment):
-            raise ValueError("Invalid comment")
     def display_full_version(self):
-        return f"Client ID: {self.client_id}, Surname: {self.surname}, First Name: {self.first_name}, Patronymic: {self.patronymic}, Comment: {self.comment}"
+        return (f"Client ID: {self.client_id}, Surname: {self.surname}, First Name: {self.first_name}, "
+                f"Patronymic: {self.patronymic}, Email: {self.email}, Phone: {self.phone_number}, "
+                f"Passport: {self.passport_number}, Comment: {self.comment}")
     
-class ClientSummary(BaseClient):
+class ShortClient(BaseClient):
     def __init__(self, client_id, surname="", first_name=""):
         super().__init__(client_id, surname, first_name)
 
+    def __eq__(self, other):
+        if not isinstance(other, BaseClient):
+            return NotImplemented
+        return (self.client_id == other.client_id and
+                self.surname == other.surname and
+                self.first_name == other.first_name)
+
     def display_summary(self):
-        return f"Client ID: {self.client_id}, Surname: {self.surname}, First Name: {self.first_name}"
-
-full_client = FullClient(client_id="client_id=1,surname=Ivanov,first_name=Ivan,patronymic=Petrovich,comment=Important client")
-print(full_client.display_full_version())
-
-client_summary = ClientSummary(client_id="client_id=1,surname=Ivanov,first_name=Ivan")
-print(client_summary.display_summary())
+        initials = f"{self.first_name[0]}." if self.first_name else ""
+        patronymic_initial = f"{self.patronymic[0]}." if self.patronymic else ""
+        return f"Client ID: {self.client_id}, Surname: {self.surname}, Initials: {initials}{patronymic_initial}"
